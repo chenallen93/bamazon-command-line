907b2e8f6362fc28a2d8ff70a599b274a0679acc
Allen Chen <allenchen@Allens-MacBook-Pro.local>
2017-06-22 16:51:56 -0400

    bamazon app

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+insert into products(productname,departmentname,price,stockquantity)
+values ("Uncharted 4","Video Games",49.95,150),
+("DOOM","Video Games",4,150),
+("League","Video Games",30,12),
+("Overwatch","Video Games",20,40),
+("hoodie","Apparel",50,20);
+select * from bamazon.products;
